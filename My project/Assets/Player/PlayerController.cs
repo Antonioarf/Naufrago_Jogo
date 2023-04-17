7903c74010673472b06069c6258bf198d19be3b9
Antonioarf <antonio.awad.roxo@gmail.com>
2023-04-17 01:23:25 -0300

    sera

---
+++ My project/Assets/Player/PlayerController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float linear_speed;
+    public float rotation_speed;
+    public float bullet_speed;
+    Vector2 movementInput;
+    public GameObject bulletPrefab; // assign your bullet prefab in the Inspector
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate() {
+        if(movementInput != Vector2.zero){
+            GetComponent<Animator>().SetBool("Andando", true);
+            rb.MovePosition(rb.position + movementInput * linear_speed * Time.fixedDeltaTime);
+            Rotate(movementInput);
+        } else {
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+            GetComponent<Animator>().SetBool("Andando", false);
+        }}
+
+
+
+    void OnMove(InputValue movementValue) {
+        movementInput = movementValue.Get<Vector2>();
+    }
+
+    void OnFire(InputValue clickValue) {
+        GetComponent<Animator>().SetTrigger("Tiro");
+        
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Vector3 direction = mousePosition - transform.position;
+        direction.z = 0f;
+        direction.Normalize();
+
+        // Create a new bullet instance and set its position and rotation
+        Quaternion playerRotation = transform.rotation;
+        Vector3 offset = playerRotation * new Vector3(0.5f, -0.5f, 0f); // adjust the offset as needed
+        GameObject bullet = Instantiate(bulletPrefab, (transform.position+offset), Quaternion.identity);
+        bullet.transform.rotation = Quaternion.LookRotation(Vector3.forward,direction);
+
+        // Shoot the bullet in the direction of the mouse cursor
+        Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+        bulletRigidbody.velocity = direction * bullet_speed;
+        }
+
+    void Rotate(Vector2 direction){ // essa eh na conta do GPT
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    }
+
