c389e3780e33f620ebdd87b18be4a5f0a95ff9bf
Guilherme Rosada <62749179+Rosada-eng@users.noreply.github.com>
2023-04-27 15:23:35 -0300

    fix player referece to catch by tag name

--- My project/Assets/Scripts/CollectableObject.cs
+++ My project/Assets/Scripts/CollectableObject.cs
@@ -4,41 +4,49 @@ using UnityEngine;
 
 public class CollectableObject : MonoBehaviour
 {
-    public GameObject player;
+    private GameObject player;
     private string collectableType;
 
     private AudioSource audioSource;
     public AudioClip collectSound;
 
 
-    void Awake(){
+    void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
         collectableType = gameObject.tag;
         audioSource = GetComponent<AudioSource>();
     }
 
-    private void DestroySelf() {
+    private void DestroySelf()
+    {
         Destroy(gameObject);
     }
 
-    public void CollisionWithPlayerDetected() {
+    public void CollisionWithPlayerDetected()
+    {
         Debug.Log("Collision with " + collectableType);
 
 
-        if (gameObject.CompareTag("Food")) {
+        if (gameObject.CompareTag("Food"))
+        {
             player.GetComponent<PlayerController>().increaseHealthPoints(10);
-        } 
-        else if (gameObject.CompareTag("Wood")) {
+        }
+        else if (gameObject.CompareTag("Wood"))
+        {
             player.GetComponent<PlayerController>().woodCollected += 1;
         }
-        else if (gameObject.CompareTag("Rope")) {
+        else if (gameObject.CompareTag("Rope"))
+        {
             player.GetComponent<PlayerController>().ropeCollected += 1;
-        } 
-        else if (gameObject.CompareTag("Fabric")) {
+        }
+        else if (gameObject.CompareTag("Fabric"))
+        {
             player.GetComponent<PlayerController>().fabricCollected += 1;
         }
 
 
-        
+
         audioSource.PlayOneShot(collectSound);
 
         Invoke("DestroySelf", 0.2f);
